7e390be156b4156fe518d9df30ab2fad51f1b829
jackRen <rf1989817000@163.com>
2019-08-25 20:12:24 +0800

    从云数据库获取歌单

---
+++ cloudfunctions/music/index.js
@@ -0,0 +1,25 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+const TcbRouter = require('tcb-router')
+cloud.init()
+
+const db = cloud.database({
+  env: 'small-5azp9'
+})
+
+
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+const app = new TcbRouter({event})
+app.router('playlist', async (ctx, next) => {
+ctx.body = await db.collection('playlist')
+.skip(event.start)
+.limit(event.count).orderBy('createTime','desc')
+.get()
+.then((res)=> {
+  return res
+})
+})
+return app.serve()
+}
