3901679e506bd68ee62af58a07be2c62515d859f
jackRen <rf1989817000@163.com>
2019-08-29 09:51:24 +0800

    search组件

---
+++ miniprogram/components/search/search.js
@@ -0,0 +1,34 @@
+// components/search/search.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+ placeholder: {
+   type: String,
+   value: '请输入关键字'
+ }
+  },
+
+  // 从外部传进来的样式
+
+  externalClasses: [
+    'iconfont',
+    'iconsousuo'
+  ],
+
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
